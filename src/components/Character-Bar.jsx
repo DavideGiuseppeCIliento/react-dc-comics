94afd4c24b98b60f054131ee7a7320862c7f1817
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-20 20:10:57 +0200

    Creato un array con i nomi delle caratteristiche  e ciclato su quelle per creare il contenuto della row.

--- src/components/Character-Bar.jsx
+++ src/components/Character-Bar.jsx
@@ -1,70 +1,41 @@
 import icon1 from "../assets/img/buy-comics-digital-comics.png";
 
+const items = [
+  "Digital Comics",
+  "DC Merchandise",
+  "Subscription",
+  "Comic Shop",
+  "DC Power Visa",
+];
+
 export default function CharacterBar() {
   return (
     <>
+      {/* CREAZIONE DELLE COLONNE CON UN MAP */}
+
       <div className="character-bar bg-primary ">
         <div className="container">
           <div className="row p-5 text-white ">
-            {/* COL 1*/}
-            <div className="col">
-              <div className="row">
-                <div className="col-md-3">
-                  <img src={icon1} alt="Icona 1" className="img-fluid" />
+            {/* COLLONNA*/}
+            {items.map((item, index) => {
+              // Restituisce tutte le colonne per ogni item nell'array
+              return (
+                <div className="col" key={index}>
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img
+                        src={icon1}
+                        alt={`Icona ${index + 1}`}
+                        className="img-fluid"
+                      />
+                    </div>
+                    <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
+                      {item}
+                    </div>
+                  </div>
                 </div>
-                <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
-                  Digital Comics
-                </div>
-              </div>
-            </div>
-
-            {/* COL 2*/}
-            <div className="col">
-              <div className="row">
-                <div className="col-md-3">
-                  <img src={icon1} alt="Icona 1" className="img-fluid" />
-                </div>
-                <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
-                  DC Merchndise
-                </div>
-              </div>
-            </div>
-
-            {/* COL 3*/}
-            <div className="col">
-              <div className="row">
-                <div className="col-md-3">
-                  <img src={icon1} alt="Icona 1" className="img-fluid" />
-                </div>
-                <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
-                  Subscription
-                </div>
-              </div>
-            </div>
-
-            {/* COL 4*/}
-            <div className="col">
-              <div className="row">
-                <div className="col-md-3">
-                  <img src={icon1} alt="Icona 1" className="img-fluid" />
-                </div>
-                <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
-                  Comic Shop
-                </div>
-              </div>
-            </div>
-
-            {/* COL 5*/}
-            <div className="col">
-              <div className="row">
-                <div className="col-md-3">
-                  <img src={icon1} alt="Icona 1" className="img-fluid" />
-                </div>
-                <div className="col-md-9 text-start d-flex align-items-center text-uppercase">
-                  DC Power Visa
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
