e05340d97f984233d9fca6acb23ca3ca77f45a2e
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-20 18:04:53 +0200

    Creata prima parte del footer con immagine

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,100 @@
 export default function Footer() {
-  return <h1>Ciao</h1>;
+  return (
+    <div className="footer background-footer">
+      <div className="container pt-5 pb-5">
+        <div className="row">
+          <div className="col-md-4 ">
+            <div className="row text-white">
+              <div className="col-4">
+                <h5>DC COMICS</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                </ul>
+                <h5>DC</h5>
+                <ul className="list-unstyled">
+                  <li>Ciao</li>
+                  <li>Ciao</li>
+                </ul>
+              </div>
+              <div className="col-4">
+                <h5>DC</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-4">
+                <h5>SITES</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                  <li>
+                    <small>ciao</small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
