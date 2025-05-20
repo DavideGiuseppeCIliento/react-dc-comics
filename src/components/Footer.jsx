92c12d064280c8b3b103117ff5f552b2db9a9a65
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-20 19:18:14 +0200

    Finito di creare il footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import facebook from "../assets/img/footer-facebook.png";
+import periscope from "../assets/img/footer-periscope.png";
+import pinterest from "../assets/img/footer-pinterest.png";
+import twitter from "../assets/img/footer-twitter.png";
+import youtube from "../assets/img/footer-youtube.png";
+
 export default function Footer() {
   return (
     <div className="footer background-footer">
@@ -7,90 +13,109 @@ export default function Footer() {
             <div className="row text-white">
               <div className="col-4">
                 <h5>DC COMICS</h5>
-                <ul className="list-unstyled">
+                <ul className="list-unstyled text-color-footer ">
                   <li>
-                    <small>ciao</small>
+                    <small>DC</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>DCstore</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Forever</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Anco Marzio</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Numa Pompilio</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Prisco</small>
                   </li>
                 </ul>
                 <h5>DC</h5>
-                <ul className="list-unstyled">
-                  <li>Ciao</li>
-                  <li>Ciao</li>
+                <ul className="list-unstyled text-color-footer ">
+                  <li>Shop</li>
+                  <li>Shop DJ</li>
                 </ul>
               </div>
               <div className="col-4">
                 <h5>DC</h5>
-                <ul className="list-unstyled">
+                <ul className="list-unstyled text-color-footer ">
                   <li>
-                    <small>ciao</small>
+                    <small>Character</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Comics</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Movies</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Tv</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Games</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Videos</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>News</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Jobs</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Subscri</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Rating</small>
                   </li>
                 </ul>
               </div>
               <div className="col-4">
                 <h5>SITES</h5>
-                <ul className="list-unstyled">
+                <ul className="list-unstyled text-color-footer ">
                   <li>
-                    <small>ciao</small>
+                    <small>Vitoantonio</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Michelangelo</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>Universe</small>
                   </li>
                   <li>
-                    <small>ciao</small>
+                    <small>heart</small>
                   </li>
                   <li>
-                    <small>ciao</small>
-                  </li>
-                  <li>
-                    <small>ciao</small>
+                    <small>Cassano</small>
                   </li>
                 </ul>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="contact-bar">
+        <div className="container pt-3 pb-3">
+          <div className="row ">
+            <div className="col">
+              <button type="button" className="btn btn-outline-light">
+                SIGN-UP NOW!
+              </button>
+            </div>
+            {/* FOLLOW US */}
+            <div className="col d-flex justify-content-end align-items-center gap-3">
+              <strong className="text-white">
+                <small>FOLLOW US:</small>
+              </strong>
+              <img src={facebook} alt="Facebook" width="25" height="25" />
+              <img src={periscope} alt="Periscope" width="25" height="25" />
+              <img src={pinterest} alt="Pinterest" width="25" height="25" />
+              <img src={twitter} alt="Twitter" width="25" height="25" />
+              <img src={youtube} alt="YouTube" width="25" height="25" />
             </div>
           </div>
         </div>
