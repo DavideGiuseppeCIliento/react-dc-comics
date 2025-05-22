503609e9e833b1d5b7c92f59ad28d95fa7c57caf
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-22 17:16:33 +0200

    Ho sistemato lo stile del button.

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,5 +1,3 @@
 export default function Button({ text }) {
-  return (
-    <button className="load-more-button bg-primary text-white ">{text}</button>
-  );
+  return <button className="load-more-button  text-white ">{text}</button>;
 }
